10287e1f9280f7367b95df19f2a43e5e9527353e
Harshad-23 <harshadgujar777@gmail.com>
2022-07-08 12:33:59 +0530

    firebase configurations

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+//App's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoeC1fcv9pHPBKxV0LmXr8Oe_b3cRa_1o",
+
+  authDomain: "watch-my-crypto.firebaseapp.com",
+
+  projectId: "watch-my-crypto",
+
+  storageBucket: "watch-my-crypto.appspot.com",
+
+  messagingSenderId: "306482826134",
+
+  appId: "1:306482826134:web:dc49c62304c7208a73abb7",
+};
+
+export default firebaseConfig;
